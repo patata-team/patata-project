1d6a30ffca04331b21c58e22a943f91c188f81bd
Luis Junco <38411763+ludovico-wd@users.noreply.github.com>
2024-11-01 11:07:12 +0100

    add some demo code

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
+import { useState } from "react";
 import Navbar from "./components/Navbar";
 
 function App() {
+
+  const [counter, setCounter] = useState(0);
+
+  const increaseCounter = () => {
+    const newValue = counter + 1;
+    setCounter(newValue)
+  }
+  
   return (
     <>
       <h1>Hello Devstructors</h1>
       <h2>Welcome to patata project</h2>
 
       <Navbar />
-      
+
+      <section className="box counter-container">
+        <h2>Number of clicks: {counter}</h2>
+        <button onClick={increaseCounter}>Click me</button>
+      </section>
     </>
   );
 }
