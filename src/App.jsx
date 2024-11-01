0943c0d19266cbe67461da9b3274d3f2922581d9
Olga Casanovas Monclus <o.casanovas.m@gmail.com>
2024-11-01 10:29:22 +0100

    some list was added

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
-
-
 function App() {
-
-
   return (
     <>
-      
       <h1>Hello Devstructors</h1>
       <h2>Welcome to patata project</h2>
-      
+      <ul>
+        <li>number one</li>
+        <li>number 2</li>
+        <li>number 3</li>
+      </ul>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
