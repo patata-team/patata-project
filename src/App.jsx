577a3c768e06191479f1c1b46e7ed34fe5448a0a
Olga Casanovas Monclus <o.casanovas.m@gmail.com>
2024-11-01 11:14:51 +0100

    give trouble back to you Luis

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,13 @@ import { useState } from "react";
 import Navbar from "./components/Navbar";
 
 function App() {
+  const [counter, setCounter] = useState(10);
 
-  const [counter, setCounter] = useState(0);
+  const reduceCounter = () => {
+    const newValue = counter - 1;
+    setCounter(newValue);
+  };
 
-  const increaseCounter = () => {
-    const newValue = counter + 1;
-    setCounter(newValue)
-  }
-  
   return (
     <>
       <h1>Hello Devstructors</h1>
@@ -19,7 +18,7 @@ function App() {
 
       <section className="box counter-container">
         <h2>Number of clicks: {counter}</h2>
-        <button onClick={increaseCounter}>Click me</button>
+        <button onClick={reduceCounter}>Click me</button>
       </section>
     </>
   );
