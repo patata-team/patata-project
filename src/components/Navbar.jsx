2ddd590297e42e869dc66277c63e733c55b4945d
Luis Junco <38411763+ludovico-wd@users.noreply.github.com>
2024-11-01 10:34:37 +0100

    refactor: extract Navbar to specific component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+function Navbar() {
+    return (
+        <ul>
+            <li>number one</li>
+            <li>number 2</li>
+            <li>number 3</li>
+        </ul>
+    )
+}
+
+export default Navbar;
